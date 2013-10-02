b947dd646da82a0bf0dfe6db6f12b70b7543d443
Fabien Basmaison <fabien@arkhi.org>
2013-10-02 23:06:59 +0800

    Filtre pour appliquer un copyright en fonction de l’URL de l’article original ajouté

--- mes_fonctions.php
+++ mes_fonctions.php
@@ -1,42 +1,49 @@
 <?php if (!defined("_ECRIRE_INC_VERSION")) return;
-/*
-* filtre pour les brèves de "traduit ailleurs"
-* -- on ne veut pas de retour à la ligne
-* -- pendant que j'y suis je nettoie tout sauf em, strong, a
-* -- de toute fa...
-* */
 
+/**
+ * Filtre pour les brèves de "traduit ailleurs"
+ * On ne veut pas de retour à la ligne.
+ * Pendant que j'y suis je nettoie tout sauf em, strong, a
+ * @param  string $str Texte à filtrer
+ * @return string      Texte filtré
+ */
 function pompage_strip_tags($str) {
   $str = strip_tags($str, '<a><em><strong>');
   return $str;
 }
 
-/*
-* test d'iso propre
-* */
-function date_iso_pompage($date_heure) {
-  list($annee, $mois, $jour) = recup_date($date_heure);
-  list($heures, $minutes, $secondes) = recup_heure($date_heure);
-  $time = mktime($heures, $minutes, $secondes, $mois, $jour, $annee);
-  return gmdate("Y-m-d\TH:i:s\+02:00", $time);
+/**
+ * Affiche le copyright correspondant à l’article traduit
+ * @param  string $url L’URL de l’article original
+ * @return string      Chaîne fournie par l’auteur ou l’éditeur
+ */
+function copyright($url) {
+  $copyright = '';
+
+  $url_copyrights = array(
+    'alistapart.com' => '
+      <span lang="en">
+        Translated with the permission of
+        <a href="http://www.alistapart.com/">A List Apart</a>
+        and the author[s].
+      </span>'
+  );
+
+  foreach($url_copyrights as $key => $value){
+    if(stristr($url, $key)){
+      $copyright = $value;
+      break;
+    }
+  }
+
+  return $copyright;
 }
 
-
-/* extra fields restrictions
-***********************************************************
-include_spip('inc/cextras_autoriser');
- restrict an array of extra fields to sector 1 (traduction)
-restreindre_extras('article', array('autorisation',
-                                    'beta',
-                                    'titre_orig',
-                                    'date_orig',
-                                    'url_orig'), 1, 'secteur');
-*/ 
-
-
-// Fabien‑20110709 : modification pour pompage
-// Echapper les <code>...</ code>
-// http://doc.spip.org/@traiter_echap_code_dist
+/**
+ * Fabien‑20110709 : modification pour pompage
+ * Echapper les <code>...</ code>
+ * http://doc.spip.org/@traiter_echap_code_dist
+ */
 function traiter_echap_code($regs) {
   list(,,$att,$corps) = $regs;
   $echap = htmlspecialchars($corps); // il ne faut pas passer dans entites_html, ne pas transformer les &#xxx; du code !
@@ -54,4 +61,3 @@ function traiter_echap_code($regs) {
   $echap = str_replace("\t", "  ", $echap);
   return $echap;
 }
-
