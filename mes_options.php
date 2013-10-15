8e5376d7b72d4ee9a65b58a1661c1455d13830d0
Fabien Basmaison <fabien@arkhi.org>
2013-10-15 23:04:43 +0800

    nouveau répertoire pour les plugins déclaré

--- mes_options.php
+++ mes_options.php
@@ -2,7 +2,8 @@
 
 /* change all characters in URL to lowercase
 ***********************************************************/
-define ('_url_minuscules',1);
+define('_url_minuscules', 1);
+define('_DIR_PLUGINS_SUPPL', _DIR_RACINE."squelettes/plugins/");
 
 /* modifies the way URL are created
 ***********************************************************/
